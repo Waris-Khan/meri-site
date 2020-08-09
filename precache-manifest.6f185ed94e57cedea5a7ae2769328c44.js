50e8d4071e7a642bce5a4b4905403de7090d6296
Waris-Khan <k.waris14@gmail.com>
2020-08-09 18:56:27 +0500

    Updates

---
+++ precache-manifest.6f185ed94e57cedea5a7ae2769328c44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8a9d459d97a3adcc651426db2f207b",
+    "url": "/meri-site/index.html"
+  },
+  {
+    "revision": "4fa4846646c462575288",
+    "url": "/meri-site/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "f1e23483e91a9875a3fa",
+    "url": "/meri-site/static/css/main.a187274e.chunk.css"
+  },
+  {
+    "revision": "4fa4846646c462575288",
+    "url": "/meri-site/static/js/2.2db2dad3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/meri-site/static/js/2.2db2dad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e23483e91a9875a3fa",
+    "url": "/meri-site/static/js/main.bf7cb492.chunk.js"
+  },
+  {
+    "revision": "4fe9d78bf3dd2be1022c",
+    "url": "/meri-site/static/js/runtime-main.6b4fe0f3.js"
+  },
+  {
+    "revision": "400a33e1df3014958e5d88640b283146",
+    "url": "/meri-site/static/media/center.400a33e1.jfif"
+  }
+]);
